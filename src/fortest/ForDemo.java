7e76bfe56e202af77f36107753a157c9b33bf41f
moon177 <545518104@qq.com>
2018-08-01 16:41:06 +0800

    gengxin

---
+++ src/fortest/ForDemo.java
@@ -0,0 +1,28 @@
+package fortest;
+
+import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author
+ * @Description:
+ * @date 2018/7/26 18:49
+ */
+public class ForDemo {
+    private static final Integer ONE = new Integer(1);//为什么加这句话
+    public static void main(String args[]) {
+        String[] args = {"aaa","bbb","ccc","aaa","ddd","ccc","ccc"};
+        Map m = new HashMap<>();
+        for (int i = 0;i<args.length;i++){//什么意思
+            Integer freq = (Integer) m.get(args[i]);//freq什么意思,这句话为什么要用这种方式人
+            m.put(args[i],(freq==null? ONE : new Integer(freq.intValue() +1)));
+        }
+        System.out.println
+                (m.size() + "distinct words detected:");
+        System.out.println(m);
+       
+    }
+
+}
